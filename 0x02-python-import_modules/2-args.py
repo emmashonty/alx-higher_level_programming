a49345bdc60368cf4bc554fd29b13e349c50c7af
Joseph Emmanuel <102731916+emmashonty@users.noreply.github.com>
2023-11-30 21:04:40 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+def add(a, b):
+    """My addition function
 
-if __name__ == "__main__":
-    """Print the number of and list of arguments."""
-    import sys
+    Args:
+        a: first integer
+        b: second integer
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+~                    
