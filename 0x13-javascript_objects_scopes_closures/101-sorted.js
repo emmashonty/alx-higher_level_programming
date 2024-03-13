aa8273d4e1e5b4b0d5aaa9cdc82c7d14925059b4
Joseph Emmanuel <102731916+emmashonty@users.noreply.github.com>
2024-03-13 19:24:08 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-const dictInput = require('./101-data').dict;
-const outDict = {};
-
-for (const key in dictInput) {
-  const ocurr = dictInput[key];
-  outDict[ocurr] = [];
-  for (const keys in dictInput) {
-    if (dictInput[keys] === ocurr) {
-      outDict[ocurr].push(keys);
-    }
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
 }
-console.log(outDict);
+console.log(newDict);
